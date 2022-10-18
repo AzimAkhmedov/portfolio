ca76bdbdbc8f1d99279c8749945c1beb4ecf4c5b
CECK <az.ah.qwerty@gmail.com>
2022-10-18 13:15:45 +0500

    Display changes

--- src/app/routers/appRouter.jsx
+++ src/app/routers/appRouter.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Layout from '../../components/Layout'
-import MainPage from '../../pages/MainPage'
+import MainPage from '../../pages/Main/MainPage'
 const appRouter = () => {
 
   return (
